af19a915a10328a7ca3394c3fddf06df4333deb1
Jas Bawa <jasmohan.bawa@gmail.com>
2017-04-24 12:49:39 -0400

    Added made up data to the parkingactivity

--- app/src/main/java/org/dvorak/cbushackidea/ParkingActivity.java
+++ app/src/main/java/org/dvorak/cbushackidea/ParkingActivity.java
@@ -52,10 +52,24 @@ public class ParkingActivity extends FragmentActivity implements OnMapReadyCallb
     //    Is called when the map is ready and basic parameters of the map are set-up. This method creates all the orange markers on the map
     public void createMarkers() {
 
+        LatLng test1 = new LatLng(32.71263, -117.16116);
+        Marker test1Marker = mMap.addMarker(new MarkerOptions().position(test1).title("Open Parking"));
+        LatLng test2 = new LatLng(32.7105, -117.15927);
+        Marker test2Marker = mMap.addMarker(new MarkerOptions().position(test2).title("Possibly Taken"));
+        LatLng test3 = new LatLng(32.71479, -117.15661);
+        Marker test3Marker = mMap.addMarker(new MarkerOptions().position(test3).title("No Parking Available"));
+
+        getInfo();
+
     }
 
-    public void getJson(){
+//    Will contact GE's servers and get the simulated data and add it to the map
+    public void getInfo(){
 
+//        HttpClient httpClient = new DefaultHttpClient();
+//        HttpGet httpGet = new httpGet("https://8553482c-1d32-4d38-8597-2e56ab642dd3.predixuaa.run.asv-pr.ice.predix.io");
+//        //Need to put in Headers
+//        //Getting simulated data is not finished. Made up data was added on to the map to show how it would act
 
 
     }
